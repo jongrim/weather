627dbfc47265c9be62a6753253781b34184eca56
Jonathan Grim <jonjongrim@gmail.com>
2017-04-26 14:08:04 -0400

    Adds city list tests

--- test_weather.py
+++ test_weather.py
@@ -8,6 +8,28 @@ class TestWeather(unittest.TestCase):
         w = Weather()
         self.assertTrue(isinstance(w, Weather))
 
+    def test_weather_has_API(self):
+        w = Weather()
+        self.assertTrue(w.api_key)
+
+    def test_weather_has_city_list(self):
+        w = Weather()
+        self.assertTrue(w.city_list)
+
+
+class TestWeatherCityList(unittest.TestCase):
+
+    def setUp(self):
+        self.w = Weather()
+
+    def test_get_id_by_city_name_where_name_is_included(self):
+        city = self.w.get_city_id_by_name('Atlanta')
+        self.assertEqual(city, 4180439)
+
+    def test_get_id_by_city_name_where_name_isnot_included(self):
+        city = self.w.get_city_id_by_name('ZZZZZ')
+        self.assertEqual(city, None)
+
 
 if __name__ == '__main__':
     unittest.main()
--- weather.py
+++ weather.py
@@ -6,10 +6,20 @@ import json
 
 
 class Weather:
-    weather_dict = {}
+    city_list = None
     api_key = ''
 
     def __init__(self):
         # build a dictionary of all the cities
         # read the api key
-        pass
+        with open('API_key.txt', 'r') as file:
+            self.api_key = file.readline()
+
+        with open('city.list.json', 'r') as file:
+            self.city_list = json.load(file)
+
+    def get_city_id_by_name(self, name):
+        for city in self.city_list:
+            if city["name"] == name:
+                return city["id"]
+        return None
