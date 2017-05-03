d6934e8c07a4258d9372a577dcbfd5e0b34b0b77
Jonathan Grim <jonjongrim@gmail.com>
2017-05-03 11:46:40 -0400

    Corrects data storage issue and refactors to clean up data storage

--- weather.py
+++ weather.py
@@ -16,7 +16,7 @@ class Weather:
     api_key = ''
     api_limit = timedelta(minutes=10)
     last_call_time = None
-    WeatherData = None
+    weather_data_dict = {}
 
     def __init__(self, last_call_time=None):
         with open('API_key.txt', 'r') as file:
@@ -26,14 +26,10 @@ class Weather:
             self.city_list = json.load(file)
 
         if os.path.isfile('weather.db'):
-            with shelve.open('weather') as file:
-                self.last_call_time = last_call_time or file['last_call_time']
+            self.restore_saved_info(last_call_time)
+
         else:
-            # open and close the shelf so it will exist going forward
-            shelfFile = shelve.open('weather')
-            shelfFile['last_call_time'] = datetime(1988, 6, 6)
-            self.last_call_time = last_call_time or shelfFile['last_call_time']
-            shelfFile.close()
+            self.make_shelf_file(last_call_time)
 
     def get_city_id_by_name(self, name):
         for city in self.city_list:
@@ -44,32 +40,45 @@ class Weather:
     def check_if_within_limit(self):
         return (datetime.now() - self.last_call_time) > self.api_limit
 
-    def store_current_time(self):
+    def make_shelf_file(self, last_call_time):
+        shelfFile = shelve.open('weather')
+        shelfFile['last_call_time'] = datetime(1988, 6, 6)
+        self.last_call_time = last_call_time or shelfFile['last_call_time']
+        shelfFile.close()
+
+    def restore_saved_info(self, last_call_time):
+            with shelve.open('weather') as file:
+                self.last_call_time = last_call_time or file['last_call_time']
+                self.weather_data_dict = file['weather_data']
+
+    def store_current_info(self):
+        '''Store the last call time and weather data for future reference'''
         now = datetime.now()
         with shelve.open('weather') as file:
             file['last_call_time'] = now
+            file['weather_data'] = self.weather_data_dict
         self.last_call_time = now
 
     def request_weather_with_id(self, id):
+        '''Request current weather conditions for the supplied city id and
+        update the instance variables last_call_time and weather_data_dict.
+        '''
         params = {'APPID': self.api_key, 'id': id}
         base_url = 'http://api.openweathermap.org/data/2.5/weather'
 
         # check last call time to rate limit
         if self.check_if_within_limit():
             response = requests.get(base_url, params=params)
-            weather_data = response.json()
-            response = response
-            WeatherData = namedtuple('WeatherData', ['json', 'response'])
-            self.WeatherData = WeatherData(weather_data, response)
-            self.store_current_time()
+            self.weather_data_dict['json'] = response.json()
+            self.weather_data_dict['response'] = response
+            self.store_current_info()
 
     def get_weather_by_id(self, city_name, forecast=False):
-        '''Links together the other methods to retrieve the weather data
+        '''Links together the other methods to retrieve the weather data.
 
-        Returns:
-            None: Returns None when the city name is not found in the city_list
-            WeatherData.json (json): The json weather data stored on the
-                object's WeatherData named tuple.
+        Raises:
+            KeyError: raised when city_name does not match any in the
+                city.list.json file.
         '''
         city_id = self.get_city_id_by_name(city_name)
         if not city_id:
@@ -79,8 +88,8 @@ class Weather:
 
     def display_weather(self):
         '''Display the json in a pleasing manner'''
-        pprint.pprint(self.WeatherData.json)
-        weather_dict = self.WeatherData.json
+        pprint.pprint(self.weather_data_dict['json'])
+        weather_dict = self.weather_data_dict['json']
         result_city = weather_dict['name']
         weather_description = weather_dict['weather'][0]['description']
         Temps_F = convert_temps(weather_dict['main'])
@@ -109,7 +118,6 @@ def convert_temps(temp_dict):
 def convert_timestamp(UTC_timestamp):
     time = datetime.fromtimestamp(UTC_timestamp).strftime('%Y-%m-%d %H:%M:%S')
     return time
-
 
 
 def main():
