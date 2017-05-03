8169918c0d803e8f7212aa176ddb12fb71a34c70
Jonathan Grim <jonjongrim@gmail.com>
2017-05-03 13:54:33 -0400

    Adds more weather settings and optional arguments for controlling indentation of output

--- weather.py
+++ weather.py
@@ -59,7 +59,7 @@ class Weather:
             file['weather_data'] = self.weather_data_dict
         self.last_call_time = now
 
-    def request_weather_with_id(self, id):
+    def request_weather_with_id(self, id, forecast):
         '''Request current weather conditions for the supplied city id and
         update the instance variables last_call_time and weather_data_dict.
         '''
@@ -73,7 +73,7 @@ class Weather:
             self.weather_data_dict['response'] = response
             self.store_current_info()
 
-    def get_weather_by_id(self, city_name, forecast=False):
+    def get_weather_by_id(self, city_name, forecast=False, indent=2):
         '''Links together the other methods to retrieve the weather data.
 
         Raises:
@@ -83,35 +83,52 @@ class Weather:
         city_id = self.get_city_id_by_name(city_name)
         if not city_id:
             raise KeyError(f'No city found matching {city_name}')
-        self.request_weather_with_id(city_id)
-        self.display_weather()
+        self.request_weather_with_id(city_id, forecast)
+        self.display_weather(forecast, indent)
 
-    def display_weather(self):
+    def display_weather(self, forecast, indent):
         '''Display the json in a pleasing manner'''
-        pprint.pprint(self.weather_data_dict['json'])
         weather_dict = self.weather_data_dict['json']
         result_city = weather_dict['name']
         weather_description = weather_dict['weather'][0]['description']
         Temps_F = convert_temps(weather_dict['main'])
         sunrise = convert_timestamp(weather_dict['sys']['sunrise'])
         sunset = convert_timestamp(weather_dict['sys']['sunset'])
+        rain = weather_dict.get('rain', None)
+        clouds = weather_dict.get('clouds', None)
+        wind = weather_dict.get('wind', None)
+        snow = weather_dict.get('snow', None)
 
-        width = 10
-        print(f'Current weather for {result_city}:')
-        print(f'Weather description: {weather_description}')
-        print(f'Temperature:')
-        print(f'  Current: {Temps_F.current:{width}}')
-        print(f'  High: {Temps_F.high:{width}}')
-        print(f'  Low: {Temps_F.low:{width}}')
-        print(f'Sunrise: {sunrise}')
-        print(f'Sunset: {sunset}')
+        space = ' ' * indent
+
+        if forecast:
+            pass
+        else:
+            print(f'Current weather for {result_city}:')
+            print(f'{space}Weather description: {weather_description}')
+            print(f'{space}Temperatures:')
+            print(f'{space}{space}Current: ', f'{Temps_F.current}'.rjust(5))
+            print(f'{space}{space}Max: ', f'{Temps_F.max}'.rjust(9))
+            print(f'{space}{space}Min: ', f'{Temps_F.min}'.rjust(9))
+            print(f"{space}Humidity: {weather_dict['main']['humidity']}%")
+            print(f'{space}Sunrise: {sunrise}')
+            print(f'{space}Sunset: {sunset}')
+            if rain:
+                print(f"{space}Rain volume for last 3 hours: {rain['3h']}")
+            if clouds:
+                print(f"{space}Cloudiness: {clouds['all']}")
+            if wind:
+                print(f"{space}Wind speed: {wind['speed']} meter/sec")
+                print(f"{space}Wind direction: {wind['deg']} degrees")
+            if snow:
+                print(f"{space}Snow volume for last 3 hours: {snow['3h']}")
 
 
 def convert_temps(temp_dict):
     current_temp_f = round((temp_dict['temp'] * (9 / 5) - 459.67), 2)
     max_temp_f = round((temp_dict['temp_max'] * (9 / 5) - 459.67), 2)
     min_temp_f = round((temp_dict['temp_min'] * (9 / 5) - 459.67), 2)
-    Temps_F = namedtuple('Temps_F', ['current', 'high', 'low'])
+    Temps_F = namedtuple('Temps_F', ['current', 'max', 'min'])
     return Temps_F(current_temp_f, max_temp_f, min_temp_f)
 
 
@@ -126,16 +143,19 @@ def main():
     parser.add_argument('city', help='The city for which you would like the \
                         weather')
     parser.add_argument('-f', '--forecast', dest='forecast',
-                        action='store_true')
+                        action='store_true', help='Specifies that the forecast \
+                        should be returned instead of current conditions.')
+    parser.add_argument('-i', '--indent', dest='indent', type=int, default=2,
+                        help='The indentation setting for output.')
     parser.add_argument('-d', '--datetime', dest='datetime',
                         action='store_true', help='For development purposes \
-                        only')
+                        only.')
     args = parser.parse_args()
     if args.datetime:
         w = Weather(datetime(1970, 1, 1))
     else:
         w = Weather()
-    w.get_weather_by_id(args.city, args.forecast)
+    w.get_weather_by_id(args.city, args.forecast, args.indent)
 
 
 if __name__ == '__main__':
